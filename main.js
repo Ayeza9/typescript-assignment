5007cb531371d04cc52686bbeb63645d448d2020
Ayeza9 <160562570+Ayeza9@users.noreply.github.com>
2024-03-08 17:17:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+// DATA TYPES:-
+// No 1. String
+var friendName = "sonia";
+console.log(friendName);
+//No 2. Number
+var houseNumber = 1644;
+console.log(houseNumber);
+// No 3. Boolean
+var married = false;
+console.log(married);
+// No 4. Null
+var empty = null;
+console.log(empty);
+// DATA STRUCTURES:-
+//Array
+var animals = ["cat", "dog", "lion", "elephant"];
+console.log(animals[2]);
+console.log(animals[2], animals[3]);
+var marks = [8, 10, 15, 19];
+console.log(marks[2]);
+var condition = [true, false, true, false];
+console.log(condition[3]);
+var fruits = ["apple", "mango", "banana"];
+fruits[2] = "orange";
+console.log(fruits);
+//CASES:-
+// camelcase
+var teachersName = "misssonia, misssadia, misstahira, missyusra"; // e.g of camelCase
+console.log(teachersName);
+//snake_case
+var teachers_name = "misssonia, misssadia, misstahira, missyusra"; // e.g of snake_case
+console.log(teachers_name);
+// PascalCase
+var TeachersName = "misssonia, misssadia, misstahira, missyusra"; // e.g of PascalCase
+console.log(TeachersName);
+var isUserExit = true; // e.g of camelCase
+var is_user_exit = false; // e.g of snake_case
+var IsUserExit = 45; // e.g of PascalCase
---
+++ main.ts
@@ -0,0 +1,59 @@
+// DATA TYPES:-
+
+// No 1. String
+let friendName: string = "sonia";
+console.log(friendName);
+
+//No 2. Number
+let houseNumber: number = 1644;
+console.log(houseNumber);
+
+// No 3. Boolean
+let married: boolean = false;
+console.log(married);
+
+// No 4. Null
+let empty: null = null;
+console.log(empty);
+
+
+
+
+
+// DATA STRUCTURES:-
+
+//Array
+let animals: string[] = ["cat", "dog", "lion", "elephant"];
+console.log(animals[2]);
+console.log(animals[2], animals[3]);
+
+let marks: number[] = [8,10,15,19];
+console.log(marks[2]);
+
+let condition: boolean[] = [true, false, true, false];
+console.log(condition[3]);
+
+let fruits: string [] = ["apple", "mango", "banana"];
+fruits[2] = "orange"
+console.log(fruits);
+
+
+
+
+//CASES:-
+
+// camelcase
+let teachersName: string = "misssonia, misssadia, misstahira, missyusra"; // e.g of camelCase
+console.log(teachersName);
+
+//snake_case
+let teachers_name: string = "misssonia, misssadia, misstahira, missyusra"; // e.g of snake_case
+console.log(teachers_name);
+
+// PascalCase
+let TeachersName: string = "misssonia, misssadia, misstahira, missyusra"; // e.g of PascalCase
+console.log(TeachersName);
+
+let isUserExit: boolean = true; // e.g of camelCase
+let is_user_exit: boolean = false; // e.g of snake_case
+let IsUserExit: number = 45; // e.g of PascalCase
